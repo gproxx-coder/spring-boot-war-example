e21db6db94fc6d03c7f71cf3d1df6db872a0b7c4
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2022-10-28 20:02:23 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Ganesh Patil | Devops Engineer | Build 1";
+        return "Ganesh Patil | Devops Engineer | Build 2";
     }
 
 }
